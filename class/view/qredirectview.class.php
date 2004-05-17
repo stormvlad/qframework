a35a4b661204267887064378956d7a09b2d5351f
carles <carles@e35ace24-6bd7-0310-b77d-a401732f5570>
2004-05-17 16:48:27 +0000

    qRedirectView added for redirect url's (uses header location)

---
+++ class/view/qredirectview.class.php
@@ -0,0 +1,54 @@
+<?php
+
+    include_once(QFRAMEWORK_CLASS_PATH . "qframework/class/view/qview.class.php");
+
+    /**
+     * Extends the original 'View' class to provide support for common operations, for example
+     * to automatically add support for locale. It is recommended
+     * that all classes that generate a view extend from this unless strictly necessary
+     */
+    class qRedirectView extends qView
+    {
+        var $_url;
+
+        /**
+         * Add function info here
+         */
+        function qRedirectView($url)
+        {
+            $this->qView();
+            $this->setUrl($url);
+        }
+
+        /**
+         * Add function info here
+         */
+        function getUrl()
+        {
+            return $this->_url;
+        }
+
+        /**
+         * Add function info here
+         */
+        function setUrl($url)
+        {
+            if (!eregi("^http://", $url))
+            {
+                $server = &qHttp::getServerVars();
+                $url = "http://" . $server->getValue("HTTP_HOST") . dirname($server->getValue("PHP_SELF") . "/" . $url);
+            }
+
+            $this->_url = $url;
+        }
+
+        /**
+         * Add function info here
+         */
+        function render()
+        {
+            header("Location: " . $this->_url);
+        }
+
+    }
+?>
