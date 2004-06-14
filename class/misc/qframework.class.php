ba0dd4a8feeaa51cb75b635e67693a60ecebd2d0
carles <carles@e35ace24-6bd7-0310-b77d-a401732f5570>
2004-06-14 14:36:43 +0000

    Added static qFramework class to obtain some values (at first moment only version)

---
+++ class/misc/qframework.class.php
@@ -0,0 +1,17 @@
+<?php
+
+    include_once(QFRAMEWORK_CLASS_PATH . "qframework/class/object/qobject.class.php");
+
+    define(QFRAMEWORK_VERSION, "0.9b");
+
+    class qFramework extends qObject
+    {
+        /**
+         * Add function info here
+         */
+        function getVersion()
+        {
+            return QFRAMEWORK_VERSION;
+        }
+    }
+?>
