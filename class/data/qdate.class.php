b7e4ddc74a5262285aa3d531b0dad0b0f010dd12
jordi <jordi@e35ace24-6bd7-0310-b77d-a401732f5570>
2004-10-23 17:53:46 +0000

    Add calculate age function.

--- class/data/qdate.class.php
+++ class/data/qdate.class.php
@@ -8,6 +8,21 @@
         {
             $this->Date($date);
         }
+        
+        /**
+        * Returns the years from a date
+        */
+        function getAge()
+        {
+            $dob      = date("Y-m-d", $this->getDate(DATE_FORMAT_UNIXTIME));
+            $ageparts = explode("-", $dob);
+            
+            // calculate age
+            $age = date("Y-m-d") - $dob;
+            
+            // return their age (or their age minus one year if it's not their birthday yet, in current year
+            return (date("nd") < $ageparts[1] . str_pad($ageparts[2], 2, '0', STR_PAD_LEFT)) ? $age -= 1 : $age;
+        }        
     }
 
 ?>
