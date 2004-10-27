b46d9de24c6e5f0d833fdaad1f438613a49abf2e
jordi <jordi@e35ace24-6bd7-0310-b77d-a401732f5570>
2004-10-27 19:16:31 +0000

    Minor bugs repaired

--- trunk/qframework/class/locale/qlocale.class.php
+++ trunk/qframework/class/locale/qlocale.class.php
@@ -562,6 +562,11 @@
             {
                 $timeStamp = mktime(substr($timeStamp, 8, 2), substr($timeStamp, 10, 2), substr($timeStamp, 12, 2), substr($timeStamp, 4, 2), substr($timeStamp, 6, 2), substr($timeStamp, 0, 4));
             }
+            
+            if ($timeStamp < 1)
+            {
+                return "";
+            }
 
             $hour        = (int) strftime("%H", $timeStamp);
             $hour2       = (int) strftime("%I", $timeStamp);
@@ -577,7 +582,7 @@
             $year2       = (int) strftime("%y", $timeStamp);
             $century     = (int) ($year / 100);
 
-            $lTime       = localtime($timeStamp, true);
+            $lTime       = localtime($timeStamp, true);            
             $offset      = str_replace("00", ":00", strftime("%z", $timeStamp));
 
             if ($lTime["tm_isdst"])
