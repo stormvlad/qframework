96d812ae61d7d98f9b28372d74057e7d4e7b98b9
carles <carles@e35ace24-6bd7-0310-b77d-a401732f5570>
2004-11-09 19:27:40 +0000

    Added functionality to form action management with a transparent way

--- class/action/qaction.class.php
+++ class/action/qaction.class.php
@@ -9,6 +9,7 @@
     {
         var $_controllerParams;
         var $_errors;
+        var $_formName;
 
         /**
          * Constructor.
@@ -21,6 +22,23 @@
             $this->qObject();
             $this->_controllerParams = &$controllerParams;
             $this->_errors           = array();
+            $this->_formName         = $this->getClassName();
+        }
+
+        /**
+        *    Add function info here
+        **/
+        function getFormName()
+        {
+            return $this->_formName;
+        }
+
+        /**
+        *    Add function info here
+        **/
+        function setFormName($name)
+        {
+            $this->_formName = $name;
         }
 
         /**
@@ -158,6 +176,176 @@
         {
             throw(new qException("qAction::performAfterValidation: This method must be implemented by child classes."));
             die();
+        }
+
+        /**
+        * Add function info here
+        */
+        function _getNormalizedStep($step)
+        {
+            $user       = &$this->_controllerParams->getUser();
+            $formValues = &$user->getAttributeRef("formValues");
+            $formName   = $this->getFormName();
+
+            if (empty($step))
+            {
+                $step = count($formValues[$formName]) - 1;
+            }
+            else if ($step < 0)
+            {
+                $step = count($formValues[$formName]) -1 + $step;
+            }
+
+            if ($step <  0)
+            {
+                $step = 0;
+            }
+
+            return $step;
+        }
+
+        /**
+        * Add function info here
+        */
+        function getFormValue($name, $step = null)
+        {
+            $user       = &$this->_controllerParams->getUser();
+            $formValues = &$user->getAttributeRef("formValues");
+            $formName   = $this->getFormName();
+            $step       = $this->_getNormalizedStep($step);
+
+            return $formValues[$formName][$step][$name];
+        }
+
+        /**
+        * Add function info here
+        */
+        function &getFormValues($step = null)
+        {
+            $user       = &$this->_controllerParams->getUser();
+            $formValues = &$user->getAttributeRef("formValues");
+            $formName   = $this->getFormName();
+            $step       = $this->_getNormalizedStep($step);
+
+            return $formValues[$formName][$step];
+        }
+
+        /**
+        * Add function info here
+        */
+        function setFormValue($name, $value, $step = null)
+        {
+            $user       = &$this->_controllerParams->getUser();
+            $formValues = &$user->getAttributeRef("formValues");
+            $formName   = $this->getFormName();
+            $step       = $this->_getNormalizedStep($step);
+
+            $formValues[$formName][$step][$name] = $value;
+
+            $user->setAttribute("formValues", $formValues);
+        }
+
+        /**
+        * Add function info here
+        */
+        function setFormValues($values, $step = null)
+        {
+            foreach ($values as $key => $value)
+            {
+                $this->setFormValue($key, $value, $step);
+            }
+        }
+
+        /**
+        * Add function info here
+        */
+        function resetFormValues()
+        {
+            $user       = &$this->_controllerParams->getUser();
+            $formValues = &$user->getAttributeRef("formValues");
+            $formName   = $this->getFormName();
+
+            $formValues[$formName] = array();
+        }
+
+        /**
+        * Add function info here
+        */
+        function save()
+        {
+            $controller = &$this->_controllerParams->getController();
+            $request    = &$this->_controllerParams->getHttpRequest();
+
+            if (!$controller->_sessionEnabled || $request->getRequestMethod() != $this->getValidationMethod())
+            {
+                return;
+            }
+
+            $user       = &$this->_controllerParams->getUser();
+            $formValues = $user->getAttribute("formValues");
+            $formName   = $this->getFormName();
+
+            if (empty($formValues))
+            {
+                $formValues = array();
+            }
+
+            if (empty($formValues[$formName]))
+            {
+                $formValues[$formName] = array();
+            }
+
+            $step = count($formValues[$formName]);
+            $formValues[$formName][$step] = array();
+
+            if ($this->getValidationMethod() == REQUEST_METHOD_GET)
+            {
+                $vars = &qHttp::getGetVars();
+            }
+            else if ($this->getValidationMethod() == REQUEST_METHOD_POST)
+            {
+                $vars = &qHttp::getPostVars();
+            }
+
+            $vars = $vars->getAsArray();
+
+            foreach ($vars as $key => $value)
+            {
+                $formValues[$formName][$step][$key] = $value;
+            }
+
+            $prevStep = $step - 1;
+
+            if ($prevStep >= 0)
+            {
+                foreach ($formValues[$formName][$prevStep] as $key => $value)
+                {
+                    if (!isset($formValues[$formName][$step][$key]))
+                    {
+                        $formValues[$formName][$step][$key] = $value;
+                    }
+                }
+            }
+
+            $user->setAttribute("formValues", $formValues);
+        }
+
+        /**
+        * Add function info here
+        */
+        function forward($actionName)
+        {
+            $controller = &$this->_controllerParams->getController();
+            $controller->forward($actionName);
+        }
+
+        /**
+        * Add function info here
+        */
+        function redirect($url)
+        {
+            $controller = &$this->_controllerParams->getController();
+            $controller->redirect($url);
         }
     }
 ?>
