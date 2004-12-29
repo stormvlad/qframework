1a6e87a531e933677c641c68ef7333b12b8fd88d
carles <carles@e35ace24-6bd7-0310-b77d-a401732f5570>
2004-12-29 15:17:09 +0000

    Added qStringHighlighter class to highlight some terms in a string

---
+++ class/data/qstringhighlighter.class.php
@@ -0,0 +1,123 @@
+<?php
+
+    include_once(QFRAMEWORK_CLASS_PATH . "qframework/class/object/qobject.class.php");
+
+    /**
+    * Add function info here
+    */
+    class qStringHighlighter extends qObject
+    {
+        var $_colors;
+
+        /**
+        * Add function info here
+        */
+        function qStringHighlighter($colors = null)
+        {
+            $this->qObject();
+            $this->_colors = $colors;
+
+            if (empty($colors))
+            {
+                $this->_colors = array("yellow", "lightpink", "aquamarine", "darkgoldenrod", "darkseagreen", "lightgreen", "rosybrown", "seagreen", "chocolate", "violet");
+            }
+        }
+
+        /**
+        * Add function info here
+        */
+        function getColors()
+        {
+            return $this->_colors;
+        }
+
+        /**
+        * Add function info here
+        */
+        function setColors($colors)
+        {
+            $this->_colors = $colors;
+        }
+
+        /**
+        * Add function info here
+        */
+        function getTerms($terms)
+        {
+            $terms = trim($terms);
+
+            if (empty($terms))
+            {
+                return array();
+            }
+
+            if (!is_array($terms))
+            {
+                $terms = explode(" ", $terms);
+            }
+
+            $totalTerms  = count($terms);
+            $totalColors = count($this->_colors);
+            $result      = array();
+
+            for ($i = 0; $i < $totalTerms; $i++)
+            {
+                $term  = trim($terms[$i]);
+                $color = $this->_colors[$i % $totalColors];
+
+                $result[] = "<span style=\"background:" . $color . "\">" . $term . "</span>";
+            }
+
+            return $result;
+        }
+
+        /**
+        * Add function info here
+        */
+        function getTermsString($terms)
+        {
+            return trim(implode(" ", $this->getTerms($terms)));
+        }
+
+        /**
+        * Add function info here
+        */
+        function highlight($str, $terms, $exactWords = false)
+        {
+            $terms = trim($terms);
+
+            if (empty($terms))
+            {
+                return $str;
+            }
+
+            if (!is_array($terms))
+            {
+                $terms = explode(" ", $terms);
+            }
+
+            $totalTerms  = count($terms);
+            $totalColors = count($this->_colors);
+
+            for ($i = 0; $i < $totalTerms; $i++)
+            {
+                $term  = trim($terms[$i]);
+                $color = $this->_colors[$i % $totalColors];
+
+                if ($exactWords)
+                {
+                    $pattern = "/(?!<.*?)(\b" . $term . "\b)(?![^<>]*?>)/si";
+                }
+                else
+                {
+                    $pattern = "/(?!<.*?)(" . $term . ")(?![^<>]*?>)/si";
+                }
+
+                $str = preg_replace($pattern, "<span style=\"background:" . $color . "\">$1</span>", $str);
+            }
+
+            return $str;
+        }
+    }
+
+?>
