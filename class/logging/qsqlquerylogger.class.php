c75d2ba6485ac1506f08674f961c16645290800f
carles <carles@e35ace24-6bd7-0310-b77d-a401732f5570>
2005-01-17 15:15:13 +0000

    Renamed qSqlLogger to qSqlQueryLogger and added qSqlPerformanceLogger to log query count and total sql execution time (I've forgotten add class files :-)

---
+++ class/logging/qsqlquerylogger.class.php
@@ -0,0 +1,36 @@
+<?php
+
+    include_once(QFRAMEWORK_CLASS_PATH . "qframework/class/logging/qlogger.class.php");
+    include_once(QFRAMEWORK_CLASS_PATH . "qframework/class/logging/qmessage.class.php");
+
+    /**
+    * qSqlLogger
+    */
+    class qSqlQueryLogger extends qLogger
+    {
+        /**
+        * Constructor
+        */
+        function qSqlQueryLogger()
+        {
+            $this->qLogger();
+
+            $this->priority     = 0;
+            $this->exitPriority = 1000;
+
+            $layout   = new qPatternLayout("%d{Y/m/d H:i:s} | %x{ip} | %x{script} | %x{queryCount} | %x{time} | %x{sql}%n");
+            $appender = new qStdoutAppender($layout);
+            $this->addAppender("stdout", $appender);
+        }
+
+        /**
+        * Constructor
+        */
+        function handler(&$obj, $args)
+        {
+            $message = new qMessage($args);
+            $this->log($message);
+        }
+    }
+
+?>
