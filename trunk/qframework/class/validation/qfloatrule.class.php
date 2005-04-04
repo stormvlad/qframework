0b97af80c78a3cf67d79a339334cf1c6133b9ace
carles <carles@e35ace24-6bd7-0310-b77d-a401732f5570>
2005-04-04 18:29:49 +0000

    Added qFloatRule class to validate float number (with decimal symbol, thousands separator, ...)

---
+++ trunk/qframework/class/validation/qfloatrule.class.php
@@ -0,0 +1,103 @@
+<?php
+
+    include_once(QFRAMEWORK_CLASS_PATH . "qframework/class/validation/qregexprule.class.php");
+
+    define("ERROR_RULE_FLOAT_FORMAT_WRONG", "error_rule_float_format_wrong");
+
+    /**
+    * FloatValidator class
+    */
+    class qFloatRule extends qRegExpRule
+    {
+        var $decimalSymbol;
+        var $thousandsSeparator;
+        var $thousandsSeparatorNullAllowed;
+
+        /**
+        * Constructor
+        */
+        function qFloatRule($decimalSymbol = ",", $thousandsSeparator = ".", $thousandsSeparatorNullAllowed = true)
+        {
+            $this->qRegExpRule("");
+
+            $this->_decimalSymbol                 = $decimalSymbol;
+            $this->_thousandsSeparator            = $thousandsSeparator;
+            $this->_thousandsSeparatorNullAllowed = $thousandsSeparatorNullAllowed;
+        }
+
+        /**
+        * Add function info here
+        */
+        function getDecimalSymbol()
+        {
+            return $this->_decimalSymbol;
+        }
+
+        /**
+        * Add function info here
+        */
+        function setDecimalSymbol($symbol)
+        {
+            $this->_decimalSymbol = $symbol;
+        }
+
+        /**
+        * Add function info here
+        */
+        function getThousandsSeparator()
+        {
+            return $this->_thousandsSeparator;
+        }
+
+        /**
+        * Add function info here
+        */
+        function setThousandsSeparator($separator)
+        {
+            $this->_thousandsSeparator = $separator;
+        }
+
+        /**
+        * Add function info here
+        */
+        function isThousandsSeparatorNullAllowed()
+        {
+            return $this->_thousandsSeparatorNullAllowed;
+        }
+
+        /**
+        * Add function info here
+        */
+        function setThousandsSeparatorNullAllowed($allowed)
+        {
+            $this->_thousandsSeparatorNullAllowed = $allowed;
+        }
+        /**
+        * Add function info here
+        */
+        function validate($value)
+        {
+            $decimalSymbol      = $this->getDecimalSymbol();
+            $thousandsSeparator = $this->getThousandsSeparator();
+            $regExp             = "^(([0-9]+(" . $decimalSymbol . "[0-9]+)?))$";
+
+            if (!empty($thousandsSeparator))
+            {
+                $regExp .= "|^([0-9]{1,3}(\\" . $thousandsSeparator . "[0-9]{3})*(" . $decimalSymbol . "[0-9]+)?)$";
+            }
+
+            $this->setRegExp($regExp);
+
+            if (parent::validate($value))
+            {
+                $this->setError(false);
+                return true;
+            }
+            else
+            {
+                $this->setError(ERROR_RULE_FLOAT_FORMAT_WRONG);
+                return false;
+            }
+        }
+    }
+?>
