d8c0459a4ea788385cd3502f687851d733b304bc
carles <carles@e35ace24-6bd7-0310-b77d-a401732f5570>
2005-09-27 17:35:04 +0000

    Added new numeric validation class to ensure non zero values

---
+++ trunk/qframework/class/validation/qnonzerorule.class.php
@@ -0,0 +1,43 @@
+<?php
+
+    include_once(QFRAMEWORK_CLASS_PATH . "qframework/class/validation/qrule.class.php");
+
+    define("ERROR_RULE_ZERO_VALUE", "error_rule_zero_value");
+
+    /**
+     * @brief Comprueba que el dato no sea zero (o vacío)
+     *
+     * @author  qDevel - info@qdevel.com
+     * @date    05/03/2005 19:22
+     * @version 1.0
+     * @ingroup validation rule
+     */
+    class qNonZeroRule extends qRule
+    {
+        /**
+         * The constructor does nothing.
+         */
+        function qNonZeroRule()
+        {
+            $this->qRule();
+        }
+
+        /**
+         * Validates the data. Does nothing here and it must be reimplemented by
+         * every child class.
+         */
+        function validate($value)
+        {
+            if (empty($value))
+            {
+                $this->setError(ERROR_RULE_ZERO_VALUE);
+                return false;
+            }
+            else
+            {
+                $this->setError(false);
+                return true;
+            }
+        }
+    }
+?>
