a362d7455ea2996db808a4d4b3fe2cf7f75ef8a9
carles <carles@e35ace24-6bd7-0310-b77d-a401732f5570>
2005-10-19 10:28:31 +0000

    Added email rule for complete email format  (Name Surname <name@domain.com>), and modified generic email validator to work with it

--- class/validation/qemailvalidator.class.php
+++ class/validation/qemailvalidator.class.php
@@ -1,10 +1,12 @@
 <?php
 
     include_once(QFRAMEWORK_CLASS_PATH . "qframework/class/validation/qvalidator.class.php");
-    include_once(QFRAMEWORK_CLASS_PATH . "qframework/class/validation/qemailformatrule.class.php");
-    include_once(QFRAMEWORK_CLASS_PATH . "qframework/class/validation/qemaildnsrule.class.php");
-
+    
     define("DEFAULT_CHECK_EMAIL_ADDRESS", false);
+    
+    define("EMAIL_SIMPLE_FORMAT", 2);
+    define("EMAIL_COMPLETE_FORMAT", 4);
+    define("EMAIL_ANY_FORMAT", EMAIL_SIMPLE_FORMAT | EMAIL_COMPLETE_FORMAT);
 
     /**
      * @brief Comprueba que se sigue un formato de email y se comprueba posteriormente online
@@ -16,16 +18,93 @@
      */
     class qEmailValidator extends qValidator
     {
-        function qEmailValidator($checkEmailAddress = DEFAULT_CHECK_EMAIL_ADDRESS)
+        var $_format;
+        
+        function qEmailValidator($checkEmailAddress = DEFAULT_CHECK_EMAIL_ADDRESS, $format = EMAIL_SIMPLE_FORMAT)
         {
             $this->qValidator();
-
-            $this->addRule(new qEmailFormatRule());
+            $this->_format = $format;
 
             if ($checkEmailAddress)
             {
+                include_once(QFRAMEWORK_CLASS_PATH . "qframework/class/validation/qemaildnsrule.class.php");
                 $this->addRule(new qEmailDnsRule());
             }
+        }
+
+        /**
+         * Add function info here
+         */
+        function _validateEmailFormat($value)
+        {
+            include_once(QFRAMEWORK_CLASS_PATH . "qframework/class/validation/qemailformatrule.class.php");
+            $rule = new qEmailFormatRule();
+
+            if (!$rule->validate($value))
+            {
+                $this->setError($rule->getError());
+                return false; 
+            }
+
+            return true;
+        }
+
+        /**
+         * Add function info here
+         */
+        function _validateEmailCompleteFormat($value)
+        {
+            include_once(QFRAMEWORK_CLASS_PATH . "qframework/class/validation/qemailcompleteformatrule.class.php");
+            $rule = new qEmailCompleteFormatRule();
+
+            if (!$rule->validate($value))
+            {
+                $this->setError($rule->getError());
+                return false; 
+            }
+
+            return true;
+        }
+        
+        /**
+         * Add function info here
+         */
+        function validate($value)
+        {
+            if ($this->_format == EMAIL_ANY_FORMAT)
+            {
+                if ($this->_validateEmailFormat($value))
+                {
+                    return parent::validate($value);
+                }
+
+                if ($this->_validateEmailCompleteFormat($value))
+                {
+                    return parent::validate($value);
+                }
+
+                return false;
+            }
+            elseif (($this->_format & EMAIL_SIMPLE_FORMAT) == EMAIL_SIMPLE_FORMAT)
+            {
+                if ($this->_validateEmailFormat($value))
+                {
+                    return parent::validate($value);
+                }
+
+                return false;
+            }
+            elseif (($this->_format & EMAIL_COMPLETE_FORMAT) == EMAIL_COMPLETE_FORMAT)
+            {
+                if ($this->_validateEmailCompleteFormat($value))
+                {
+                    return parent::validate($value);
+                }
+
+                return false;
+            }
+
+            return true;
         }
     }
 ?>
