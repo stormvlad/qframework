29dc1dc8a14c250aa99c359ba44b41fec74f44bf
carles <carles@e35ace24-6bd7-0310-b77d-a401732f5570>
2006-03-28 12:17:06 +0000

    Added Soap classes (client & server wrappers from PEAR Soap classes)

---
+++ trunk/qframework/class/soap/qsoapserver.class.php
@@ -0,0 +1,65 @@
+<?php
+
+    // PEAR include 
+    include_once("SOAP/Server.php");
+    
+    include_once(QFRAMEWORK_CLASS_PATH . "qframework/class/object/qobject.class.php");
+
+    /**
+    * Add class info here
+    */
+    class qSoapServer extends qObject
+    {
+        var $_soapServer;
+
+        /**
+        * Constructor
+        */
+        function qSoapServer($encoding = null)
+        {
+            $this->qObject();
+
+            if (empty($encoding))
+            {
+                include_once(APP_ROOT_PATH . "class/locale/locale.class.php");
+                $locale = &Locale::getInstance();
+                $encoding = $locale->getCharset();
+            }
+
+            // SOAP server initialization
+            $this->_soapServer = new SOAP_Server();
+            $this->_soapServer->xml_encoding      = $encoding;
+            $this->_soapServer->response_encoding = $encoding;
+
+            $this->_soapServer->addObjectMap($this, "urn:" . $this->getClassName());
+        }
+
+        /**
+        * Add function info here
+        */
+        function isError($obj)
+        {
+            return PEAR::isError($obj);
+        }
+        
+        /**
+        * Add function info here
+        */
+        function run()
+        {
+            include_once(QFRAMEWORK_CLASS_PATH . "qframework/class/net/qhttp.class.php");
+            $server = &qHttp::getServerVars();
+
+            if ($server->getValue("REQUEST_METHOD") == "POST")
+            {
+                $this->_soapServer->service($GLOBALS["HTTP_RAW_POST_DATA"]);
+            }
+            else
+            {
+                throw(new qException("qSoapServer::run: Invalid request method. Only request POST method is available."));
+                die();
+            }
+        }
+    }
+
+?>
