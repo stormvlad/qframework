f2263791ee102b4357b6dbf37096b722a7ad5984
carles <carles@e35ace24-6bd7-0310-b77d-a401732f5570>
2006-03-28 12:17:06 +0000

    Added Soap classes (client & server wrappers from PEAR Soap classes)

---
+++ class/soap/qsoapclient.class.php
@@ -0,0 +1,160 @@
+<?php
+
+    // PEAR include 
+    include_once("SOAP/Client.php");
+
+    include_once(QFRAMEWORK_CLASS_PATH . "qframework/class/object/qobject.class.php");
+
+    define("SOAP_CLIENT_DEFAULT_TIMEOUT", 120);
+    
+    /**
+    * Add class info here
+    */
+    class qSoapClient extends qObject
+    {
+        var $_soapClient;
+        var $_nameSpace;
+
+        /**
+        * Constructor
+        */
+        function qSoapClient($serverUrl, $nameSpace, $encoding = null, $timeOut = SOAP_CLIENT_DEFAULT_TIMEOUT)
+        {
+            $this->qObject();
+
+            if (empty($encoding))
+            {
+                include_once(APP_ROOT_PATH . "class/locale/locale.class.php");
+                $locale = &Locale::getInstance();
+                $encoding = $locale->getCharset();
+            }
+            
+            // SOAP client initialization
+            $this->_nameSpace  = $nameSpace;
+
+            $this->_soapClient = new SOAP_Client($serverUrl);
+            $this->_soapClient->setEncoding($encoding);
+
+            $this->setOption("timeout", $timeOut);
+
+            if (eregi("^https", $serverUrl))
+            {
+                $this->setOption("curl", CURLOPT_VERBOSE, 1);
+                $this->setOption("curl", CURLOPT_SSL_VERIFYPEER, false);
+                $this->setOption("curl", CURLOPT_SSL_VERIFYHOST, false);
+            }
+        }
+
+        /**
+        * Add function info here
+        */
+        function getServerUrl()
+        {
+            return $this->_soapClient->_endpoint;
+        }
+
+        /**
+        * Add function info here
+        */
+        function setServerUrl($url)
+        {
+            $this->_soapClient->_endpoint = $url;
+        }
+        
+        /**
+        * Add function info here
+        */
+        function getNameSpace()
+        {
+            return $this->_nameSpace;
+        }
+
+        /**
+        * Add function info here
+        */
+        function setNameSpace($name)
+        {
+            $this->_nameSpace = $space;
+        }
+        
+        /**
+        * Add function info here
+        */
+        function getEncoding()
+        {
+            return $this->_soapClient->_encoding;
+        }
+
+        /**
+        * Add function info here
+        */
+        function setEncoding($encoding)
+        {
+            $this->_soapClient->setEncoding($encoding);
+        }
+
+        /**
+        * Add function info here
+        */
+        function getTimeout()
+        {
+            return $this->_soapClient->_options["timeout"];
+        }
+
+        /**
+        * Add function info here
+        */
+        function setTimeout()
+        {
+            $this->_soapClient->setOpt("timeout", $timeOut);
+        }
+
+        /**
+        * Add function info here
+        */
+        function getOption($option)
+        {
+            return $this->_soapClient->_options[$option];
+        }
+
+        /**
+        * Add function info here
+        */
+        function setOption($option, $value)
+        {
+            $this->_soapClient->setOpt($option, $value);
+        }
+        
+        /**
+        * Add function info here
+        */
+        function &getInstance()
+        {
+            static $soapClient;
+
+            if (!isset($soapClient))
+            {
+                $soapClient = new SoapDbClient();
+            }
+
+            return $soapClient;
+        }
+
+        /**
+        * Add function info here
+        */
+        function isError($obj)
+        {
+            return PEAR::isError($obj);
+        }
+
+        /**
+        * Add function info here
+        */
+        function call($method, $params)
+        {
+            return $this->_soapClient->call($method, $params, $this->_nameSpace);
+        }
+    }
+
+?>
